9b924dbd44bc9deec0d3b2375b8972a933003abd
Jonatas <djonatas31@gmail.com>
2024-02-20 20:53:03 -0300

    feat: finish challenge

---
+++ 8Kyu/RemoveStringSpaces/RemoveStringSpaces.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(NoSpace("    dkawodkowa   8988888    dwaokdwoa"))
+}
+
+
+func NoSpace(word string) string {
+	newWord := strings.Replace(word, " ", "", -1)
+	return newWord
+}
