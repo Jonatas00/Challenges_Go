8ecb6cb884332e4d82b8f71979e26ec542ecddc4
Jonatas <djonatas31@gmail.com>
2024-02-20 20:32:34 -0300

    feat: finish test with best pratices

--- 8Kyu/SquareNSum/SquareNSum.go
+++ 8Kyu/SquareNSum/SquareNSum.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2+ 2^2 = 9
 
-func SquareSum(numbers []int) int {
-	return 0
+func main() {
+	fmt.Println(SquareSum([]int{1, 2, 2}))
+}
+
+func SquareSum(numbers []int) (resp int) {
+	for _, n := range numbers {
+		resp += n * n
+	} 
+	return resp
 }
