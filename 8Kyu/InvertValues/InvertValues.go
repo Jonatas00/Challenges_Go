2da15e06f4bd9f177b9e85b99b8bec7681531ef4
Jonatas <djonatas31@gmail.com>
2024-02-20 22:03:04 -0300

    feat: finish challenge

--- 8Kyu/InvertValues/InvertValues.go
+++ 8Kyu/InvertValues/InvertValues.go
@@ -1,6 +1,8 @@
 package main
 
-// Given a set of numbers, return the additive inverse of each. 
+import "fmt"
+
+// Given a set of numbers, return the additive inverse of each.
 // Each positive becomes negatives, and the negatives become positives.
 
 // invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
@@ -8,5 +10,15 @@ package main
 // invert([]) == []
 
 func main() {
-	
+	numbers := []int{3, 2, -1, 4}
+	fmt.Println(Invert(numbers))
+	fmt.Println(numbers)
+}
+
+func Invert(arr []int) []int {
+	var newArr []int
+	for _, v := range arr {
+		newArr = append(newArr, v * -1)
+	}
+  return newArr
 }
