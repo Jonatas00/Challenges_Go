bda8cba49ad9d69560ec9164eee2b3908df15e0f
Jonatas <djonatas31@gmail.com>
2024-02-26 21:39:01 -0300

    fix: add challenge rules

--- 8Kyu/MultipleOfIndex/MultipleOfIndex.go
+++ 8Kyu/MultipleOfIndex/MultipleOfIndex.go
@@ -1,5 +1,8 @@
 package main
 
+// Return a new array consisting of elements which are multiple of their own 
+// index in input array (length > 1).
+
 func main() {
 	multipleOfIndex([]int{22, -6, 32, 82, 9, 25})
 	multipleOfIndex([]int{68, -1, 1, -7, 10, 10})
